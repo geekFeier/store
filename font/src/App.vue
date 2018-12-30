0b48cb4cf1d572b775d38db13bc1fbccfc89ddf2
fanux <fhtjob@hotmail.com>
2018-12-30 12:15:47 +0800

    add iview

--- font/src/App.vue
+++ font/src/App.vue
@@ -1,27 +1,77 @@
 <template>
   <div id="app">
-        <HelloWorld msg="Welcome to Sealyun"/>
+    <Menu mode="horizontal" :theme="theme1" active-name="1">
+      <Row>
+        <Col span="19">
+        <MenuItem name="1">
+          <a target="_blank" href="http://store.lameleg.com">
+            <li>SealYun</li>
+          </a>
+        </MenuItem>
+        <MenuItem name="2">
+          <a target="_blank" href="https://www.sealyun.com">
+           <li>Blog</li>
+          </a>
+        </MenuItem>
+        <MenuItem name="3">
+          <a target="_blank" href="http://www.sealyun.com/pro/products">
+            <li>Docs</li>
+          </a>
+        </MenuItem>
+        </Col>
+        <Col span="5">
+        <MenuItem name="4">
+          <Tooltip content="分享收入可提现金额">
+            <li>
+              <i class="ivu-icon ivu-icon-logo-yen">
+                <Badge :count="100">&nbsp;&nbsp;&nbsp;&nbsp;</Badge>
+              </i>
+            </li>
+          </Tooltip>
+        </MenuItem>
+        <MenuItem name="5">
+          <Tooltip content="请使用github账户登录">
+            <a
+              href="https://github.com/login/oauth/authorize?client_id=89c1b05d77fb1c92a1ef&scope=user:email"
+              target="_blank"
+              rel="noopener"
+            >
+              <li>
+                <i class="ivu-icon ivu-icon-logo-github"></i>
+                Login
+              </li>
+            </a>
+          </Tooltip>
+        </MenuItem>
+        </Col>
+      </Row>
+    </Menu>
+    <HelloWorld msg="Welcome to SealYun"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  data () {
+    return {
+      theme1:'light'
+    }
+  },
+  name: "app",
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- font/src/components/HelloWorld.vue
+++ font/src/components/HelloWorld.vue
@@ -2,22 +2,19 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      为了能记录您已经购买了哪些产品，点击连接时会跳转到github进行第三方登录<br>
-      网站丑的原因： 相信客户不会为网站漂亮买单<br>
-      通过用户专有的分享连接交易完成的商品，用户分成60%<br>
-      支付成功后会自动跳转到下载安装包<br>
+      kubernetes生态应用市场<br>
+      容器可复用软件销售平台<br>
+      kubernetes集群离线安装包，一键安装，HA安装<br>
       <!--a href="https://github.com/login/oauth/authorize?client_id=89c1b05d77fb1c92a1ef&scope=user:email" target="_blank" rel="noopener">login github</a-->.
     </p>
     <h3>商品列表</h3>
     <div id="pro-link">
     <ul>
-      <li><a :href="time" rel="noopener">kubernetes1.13.1</a> 分享链接（开发中）</li>
+      <li><a :href="time" rel="noopener">kubernetes1.13.1 (元旦一折优惠中 5元)</a></li>
     </ul>
     </div>
     <h3>LINKS</h3>
     <ul>
-      <li><a href="https://sealyun.com/pro/products/" target="_blank" rel="noopener">安装文档与常见问题</a></li><br/>
-      <li><a href="https://sealyun.com" target="_blank" rel="noopener">博客</a></li><br/>
       <li><img src="https://sealyun.com/kubernetes-qrcode.jpg" /></li><br/>公众号<br/>
       <li>联系方式：fhtjob@hotmail.com</li>
     </ul>
@@ -25,16 +22,7 @@
 </template>
 
 <script  type="text/javascript">
-
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  data: function(){
-    return {time: 'http://store.lameleg.com:8080/pro/kubernetes1.13.1?time=' + (new Date()).getTime()}
-  }
-}
+export default {};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
