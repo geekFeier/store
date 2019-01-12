227a73f0ecf3c030d3952956aa539b8df7d9f9ba
fanux <fhtjob@hotmail.com>
2019-01-12 13:39:23 +0800

    hide real download url

--- store/server/product.go
+++ store/server/product.go
@@ -12,6 +12,9 @@ func productList(request *restful.Request, response *restful.Response) {
 	if err != nil {
 		response.WriteEntity(&Res{1, "list products failed"})
 	}
+	for i := range ps {
+		ps[i].ProductURL = "www.sealyun.com"
+	}
 	response.WriteEntity(&ps)
 }
 
